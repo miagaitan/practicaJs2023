c672e09aaa5eafb5d5fbd7b4859da0abe7805527
Nicolas Nocete <nicolasnocete@gmail.com>
2023-04-05 06:59:23 -0300

    ejercicios clase 04/04

---
+++ ejercicio6.js
@@ -0,0 +1,13 @@
+// Enumerar valores entre dos variables
+const a = 10;
+const b = 20;
+
+for (let i = a + 1; i <= b; i++) {
+  console.log(i);
+}
+
+let i = a + 1;
+while (i < b) {
+  console.log(i);
+  i++;
+}
